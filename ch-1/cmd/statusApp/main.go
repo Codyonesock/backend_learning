303b07bff9a82cc6d8749c9a0f04d75396c9f2c2
Cody Denny <codyonesock+github@gmail.com>
2025-04-06 17:43:33 -0300

    Fixing some linting errors

--- ch-1/cmd/statusApp/main.go
+++ ch-1/cmd/statusApp/main.go
@@ -1,3 +1,5 @@
+// Package main initializes the logger and routing.
+// It also loads a config to configure the port and stream url
 package main
 
 import (
@@ -5,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
@@ -13,42 +16,59 @@ import (
 	"github.com/codyonesock/backend_learning/ch-1/internal/status"
 )
 
-type Config struct {
+const (
+	readTimeout  = 10 * time.Second
+	writeTimeout = 10 * time.Second
+	idleTimeout  = 10 * time.Second
+)
+
+type config struct {
 	Port      string `json:"port"`
 	StreamURL string `json:"stream_url"`
 }
 
-func LoadConfig(filename string, logger *zap.Logger) (*Config, error) {
+func loadConfig(filename string, logger *zap.Logger) (*config, error) {
 	data, err := os.ReadFile("config.json")
 	if err != nil {
 		logger.Error("Error reading config file", zap.String("filename", filename), zap.Error(err))
+
 		return nil, fmt.Errorf("error reading config file: %w", err)
 	}
 
-	var config Config
+	var config config
 	err = json.Unmarshal(data, &config)
+
 	if err != nil {
 		logger.Error("Error unmarshalling JSON", zap.Error(err))
+
 		return nil, fmt.Errorf("error unmarshalling JSON: %w", err)
 	}
 
 	logger.Info("Config loaded", zap.String("port", config.Port), zap.String("stream_url", config.StreamURL))
+
 	return &config, nil
 }
 
 func main() {
 	logger, err := zap.NewProduction()
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to initalize logger: %v\n", err)
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "Failed to initialize logger: %v\n", err)
 	}
-	defer logger.Sync()
+
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to flush logger: %v\n", err)
+		}
+	}()
 
 	logger.Info("Starting StatusApp")
 
-	config, err := LoadConfig("config.json", logger)
+	config, err := loadConfig("config.json", logger)
+
 	if err != nil {
 		logger.Fatal("Error loading config", zap.Error(err))
+
 		return
 	}
 
@@ -63,9 +83,16 @@ func main() {
 	})
 
 	logger.Info("Server running", zap.String("port", config.Port))
-	if err := http.ListenAndServe(config.Port, r); err != nil {
+	server := &http.Server{
+		Addr:         config.Port,
+		Handler:      r,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeout,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
 		logger.Fatal("Error starting server", zap.Error(err))
-		os.Exit(1)
 	}
 }
 
